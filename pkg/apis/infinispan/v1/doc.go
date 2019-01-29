a0086c24ec435fd78e2859bafd255beb5d41319e
gustavonalle <gustavonalle@gmail.com>
2019-01-29 13:26:13 +0000

    Code relocation from github.com/rigazilla/infinispan-operator

---
+++ pkg/apis/infinispan/v1/doc.go
@@ -0,0 +1,4 @@
+// Package v1 contains API Schema definitions for the infinispan v1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=infinispan.org
+package v1
---
+++ pkg/apis/infinispan/v1/infinispan_types.go
@@ -0,0 +1,42 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// InfinispanSpec defines the desired state of Infinispan
+type InfinispanSpec struct {
+	Size        int32  `json:"size"`
+	ClusterName string `json:"clustername"`
+}
+
+// InfinispanStatus defines the observed state of Infinispan
+type InfinispanStatus struct {
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	Nodes []string `json:"nodes"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Infinispan is the Schema for the infinispans API
+// +k8s:openapi-gen=true
+type Infinispan struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   InfinispanSpec   `json:"spec,omitempty"`
+	Status InfinispanStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// InfinispanList contains a list of Infinispan
+type InfinispanList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Infinispan `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Infinispan{}, &InfinispanList{})
+}
