0e2ad255055c8de424e7bd58c0a39b64d01f424a
Gustavo Fernandes <gustavonalle@gmail.com>
2019-02-04 10:10:23 +0000

    Add new "config" element to the resource definition (#6)

--- pkg/apis/infinispan/v1/infinispan_types.go
+++ pkg/apis/infinispan/v1/infinispan_types.go
@@ -16,6 +16,35 @@ type InfinispanStatus struct {
 	Nodes []string `json:"nodes"`
 }
 
+type Config struct {
+	Name string `json:"name"`
+}
+
+type ConfigMapConfig struct {
+	Name string `json:"name"`
+}
+
+type ConfigSource string
+
+const (
+	// Internal means a configuration already shipped with Infinispan
+	Internal ConfigSource = "Internal"
+
+	ConfigMap ConfigSource = "ConfigMap"
+)
+
+// InfinispanConfig defines the profile (configuration) to start the cluster
+type InfinispanConfig struct {
+	// The path to the xml of the config to use, e.g. "cloud.xml". This must be relative to "/opt/jboss/infinispan-server/standalone/configuration"
+	Name string `json:"name,omitempty"`
+
+	// Where to find the config
+	SourceType ConfigSource `json:"sourceType,omitempty"`
+
+	// If the source is external, the reference to the resource
+	SourceRef string `json:"sourceRef,omitempty"`
+}
+
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
@@ -25,6 +54,7 @@ type Infinispan struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
+	Config InfinispanConfig `json:"config,omitempty"`
 	Spec   InfinispanSpec   `json:"spec,omitempty"`
 	Status InfinispanStatus `json:"status,omitempty"`
 }
