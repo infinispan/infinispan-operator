1562874dedbb5dbbe5c8eccae018c1179d4c592d
Vittorio Rigamonti <vrigamon@redhat.com>
2019-03-27 17:43:56 +0100

    removed jgroups stack configurability

--- pkg/apis/infinispan/v1/infinispan_types.go
+++ pkg/apis/infinispan/v1/infinispan_types.go
@@ -8,7 +8,6 @@ import (
 type InfinispanSpec struct {
 	Size  int32  `json:"size"`
 	Image string `json:"image"`
-	JGroupsStack string `json:"jgroups-stack"`
 }
 
 // InfinispanStatus defines the observed state of Infinispan
