c0320b1c5c55024d0e73e7cb08d4ce702874131f
Galder Zamarreño <galder@zamarreno.com>
2019-04-03 16:31:37 +0200

    Simplify scripts and add more info to README

--- operatorhub/build/install-marketplace.sh
+++ operatorhub/build/install-marketplace.sh
@@ -3,12 +3,11 @@
 set -e -x
 
 TMP_DIR=${1}
-SKIP_ERROR=${2-false}
 
 (
     cd ${TMP_DIR}/operator-marketplace
 
     # Set --validate=false to workaround:
     # https://github.com/operator-framework/operator-marketplace/issues/142
-    kubectl apply -f deploy/upstream --validate=false || ${SKIP_ERROR}
+    kubectl apply -f deploy/upstream --validate=false
 )
