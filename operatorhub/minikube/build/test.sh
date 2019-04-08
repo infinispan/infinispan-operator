c193d5dd28f148ed4925eddfddde137aee8e6f4d
Galder Zamarreño <galder@zamarreno.com>
2019-04-08 16:00:10 +0200

    Set Infinispan operator version to test on minikube

--- operatorhub/minikube/build/test.sh
+++ operatorhub/minikube/build/test.sh
@@ -3,7 +3,8 @@
 set -e -x
 
 NAMESPACE=${1}
+VERSION=${2}
 
 kubectl apply \
-    -f https://raw.githubusercontent.com/infinispan/infinispan-operator/0.2.1/deploy/cr/cr_minimal.yaml \
+    -f https://raw.githubusercontent.com/infinispan/infinispan-operator/${VERSION}/deploy/cr/cr_minimal.yaml \
     -n ${NAMESPACE}
