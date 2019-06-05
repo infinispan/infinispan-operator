2dd2af0a808e4b511310f953324adab4b5bfde41
Galder Zamarreño <galder@zamarreno.com>
2019-06-05 11:34:54 +0200

    Run CI against Minikube and OpenShift #50
    * Make OpenShift build download oc client for debugging.
    * Minikube requires xenial Ubuntu distribution to work,
    but this distro does not work well with the OpenShift build.
    * Hence, made the distro configuration a specific option
    of the Minikube stage.

--- ci/start-okd-4.0.0.sh
+++ ci/start-okd-4.0.0.sh
@@ -9,6 +9,24 @@ function get_master {
 SLEEP=3
 MAX_WAIT=100
 
+OSE_MAIN_VERSION=v3.11.0
+OSE_SHA1_VERSION=0cbc58b
+
+function download_oc_client {
+  echo "==== Installing OC Client ===="
+  if [[ -f ./oc ]]; then
+    echo "oc client installed"
+  else
+    wget -q -N https://github.com/openshift/origin/releases/download/$OSE_MAIN_VERSION/openshift-origin-client-tools-$OSE_MAIN_VERSION-$OSE_SHA1_VERSION-linux-64bit.tar.gz
+    tar -zxf openshift-origin-client-tools-$OSE_MAIN_VERSION-$OSE_SHA1_VERSION-linux-64bit.tar.gz
+    cp openshift-origin-client-tools-$OSE_MAIN_VERSION-$OSE_SHA1_VERSION-linux-64bit/oc .
+    rm -rf openshift-origin-client-tools-$OSE_MAIN_VERSION+$OSE_SHA1_VERSION-linux-64bit
+    rm -rf openshift-origin-client-tools-$OSE_MAIN_VERSION-$OSE_SHA1_VERSION-linux-64bit.tar.gz
+  fi
+}
+
+download_oc_client
+
 echo "Waiting for OKD to be ready..."
 
 while ! curl -k -s "https://$(get_master):8443/healthz/ready"
@@ -17,3 +35,4 @@ do
     ((c++)) && ((c==$MAX_WAIT)) && exit 1
     sleep $SLEEP
 done
+
