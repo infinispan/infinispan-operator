44caa05c1dbc5ea2c7d7859fb5392b47fe6f6cbf
Galder Zamarreño <galder@zamarreno.com>
2019-08-12 17:01:13 +0200

    Release script: operatorhub changes

--- build/release.sh
+++ build/release.sh
@@ -28,12 +28,20 @@ validate() {
      echo "Env variable GITHUB_USERNAME, which sets the Git Hub username is unset or set to the empty string"
      exit 1
   fi
+
+  if ! [ -x "$(command -v hub)" ]; then
+    echo 'Command line tool hub is not installed. Required to send PRs for OperatorHub manifest changes.'
+    exit 1
+  fi
 }
 
 
 branch() {
   git branch -D Release_${RELEASE_NAME} || true
   git checkout -b Release_${RELEASE_NAME}
+
+  OPERATORHUB_UPSTREAM_BRANCH="infinispan-upstream-${RELEASE_NAME}"
+  OPERATORHUB_COMMUNITY_BRANCH="infinispan-community-${RELEASE_NAME}"
 }
 
 
@@ -63,43 +71,20 @@ push() {
 
 
 cleanup() {
-  git checkout ${CURRENT_BRANCH}
   git branch -D Release_${RELEASE_NAME} || true
   rm -f deploy/*.backup
 }
 
 
-#manifests() {
-#  local csvFileName="infinispan-operator.clusterserviceversion.yaml"
-#  local releaseCsvFileName="infinispan-operator.v${RELEASE_NAME}.clusterserviceversion.yaml"
-#  local releaseCsvFilePath=${BUILD_MANIFESTS_DIR}/${releaseCsvFileName}
-#  local releasePackageFilePath=${BUILD_MANIFESTS_DIR}/infinispan.package.yaml
-#
-#  cp deploy/olm-catalog/* ${BUILD_MANIFESTS_DIR}
-#  mv ${BUILD_MANIFESTS_DIR}/${csvFileName} ${releaseCsvFilePath}
-#
-#  sed -i'.backup' "s/9.9.9/${RELEASE_NAME}/g" ${releaseCsvFilePath}
-#  sed -i'.backup' "s/9.9.8/${REPLACES_RELEASE_NAME}/g" ${releaseCsvFilePath}
-#  sed -i'.backup' "s/infinispan-server:latest/infinispan-server:${SERVER_VERSION}/g" ${releaseCsvFilePath}
-#  sed -i'.backup' "s/infinispan-operator:latest/infinispan-operator:${RELEASE_NAME}/g" ${releaseCsvFilePath}
-#
-#  local now="$(date +"%Y-%m-%dT%H:%M:%SZ")"
-#  sed -i'.backup' "s/2000-01-01T12:00:00Z/${now}/g" ${releaseCsvFilePath}
-#
-#  sed -i'.backup' "s/9.9.9/${RELEASE_NAME}/g" ${releasePackageFilePath}
-#}
-
-
 operatorhub() {
-  local repoDir=build/community-operators
+  local repoDir=build/_output/community-operators
   local upstreamDir=upstream-community-operators/infinispan
   local communityDir=community-operators/infinispan
-  local upstreamBranch="infinispan_upstream_${RELEASE_NAME}"
-  local communityBranch="infinispan_community_${RELEASE_NAME}"
 
+  rm -drf ${repoDir} || echo "Operatorhub repo does not exist"
   git clone git@github.com:${GITHUB_USERNAME}/community-operators.git ${repoDir}
-  prepareBranches ${repoDir} ${upstreamBranch} ${upstreamDir}
-  prepareBranches ${repoDir} ${communityBranch} ${communityDir}
+  prepareBranches ${repoDir} ${OPERATORHUB_UPSTREAM_BRANCH} ${upstreamDir}
+  prepareBranches ${repoDir} ${OPERATORHUB_COMMUNITY_BRANCH} ${communityDir}
 }
 
 
@@ -112,12 +97,18 @@ prepareBranches() {
   local packageFile="infinispan.package.yaml"
   local packagePath=${repoDir}/${dir}/${packageFile}
 
+  pushd ${repoDir}
+  git branch -D ${branch} || echo "Operator Hub branch exists"
   git checkout -b ${branch}
-  cp deploy/olm-catalog/${csvFile} ${csvPath}
+  popd
+
+  git checkout ${CURRENT_BRANCH}
+
+  cp deploy/olm-catalog/${CSV_FILE} ${csvPath}
   cp deploy/olm-catalog/${packageFile} ${packagePath}
 
   pushd ${repoDir}
-  updateCsvFile ${dir} ${csvFile}
+  updateCsvFile ${dir}
   git commit -a -m "Copy Infinispan manifests for ${RELEASE_NAME} release"
   popd
 
@@ -153,18 +144,31 @@ updatePackageFile() {
 }
 
 
+sendPRs() {
+  git checkout OPERATORHUB_UPSTREAM_BRANCH
+  git push origin OPERATORHUB_UPSTREAM_BRANCH
+  hub pull-request -m "[upstream] Updated Infinispan Operator to ${RELEASE_NAME}"
+
+  git checkout OPERATORHUB_COMMUNITY_BRANCH
+  git push origin OPERATORHUB_COMMUNITY_BRANCH
+  hub pull-request -m "[community] Updated Infinispan Operator to ${RELEASE_NAME}"
+}
+
+
 main() {
   validate
   branch
   replace
   commit
   tag
+  operatorhub
   cleanup
 
   if [[ "${DRY_RUN}" = true ]] ; then
     echo "DRY_RUN is set to true. Skipping..."
   else
     push
+    sendPRs
   fi
 }
 
