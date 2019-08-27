e8b5f80d9e26bbdf2ef7ee4718f32e86855e25e3
Galder Zamarreño <galder@zamarreno.com>
2019-08-27 09:58:02 +0200

    Production code changes to satisfy Go linter #120

--- pkg/controller/infinispan/util/cluster.go
+++ pkg/controller/infinispan/util/cluster.go
@@ -8,8 +8,10 @@ import (
 
 var log = logf.Log.WithName("cluster_util")
 
+// ClusterMembers is a lambda function for getting cluster members from server
 type ClusterMembers func(secretName, podName, namespace string) ([]string, error)
 
+// GetPassword returns password associated with a user in a given secret
 func GetPassword(user, secretName, namespace string) (string, error) {
 	secret, err := GetSecret(secretName, namespace)
 	if err != nil {
@@ -25,6 +27,7 @@ func GetPassword(user, secretName, namespace string) (string, error) {
 	return pass, nil
 }
 
+// GetClusterSize returns the size of the cluster as seen by a given pod
 func GetClusterSize(secretName, podName, namespace string) (int, error) {
 	members, err := GetClusterMembers(secretName, podName, namespace)
 	if err != nil {
@@ -34,17 +37,19 @@ func GetClusterSize(secretName, podName, namespace string) (int, error) {
 	return len(members), nil
 }
 
+// ClusterHealth represents the health of the cluster
 type ClusterHealth struct {
 	Nodes []string `json:"node_names"`
 }
 
+// Health represents the health of an Infinispan server
 type Health struct {
 	ClusterHealth ClusterHealth `json:"cluster_health"`
 }
 
-// GetClusterMembers get the cluster members
+// GetClusterMembers get the cluster members as seen by a given pod
 func GetClusterMembers(secretName, podName, namespace string) ([]string, error) {
-	podIp, err := GetPodIp(podName, namespace)
+	podIP, err := GetPodIP(podName, namespace)
 	if err != nil {
 		return nil, err
 	}
@@ -54,11 +59,11 @@ func GetClusterMembers(secretName, podName, namespace string) ([]string, error) 
 		return nil, err
 	}
 
-	httpUrl := fmt.Sprintf("http://%v:11222/rest/v2/cache-managers/DefaultCacheManager/health", podIp)
-	commands := []string{"curl", "-u", fmt.Sprintf("operator:%v", pass), httpUrl}
+	httpURL := fmt.Sprintf("http://%v:11222/rest/v2/cache-managers/DefaultCacheManager/health", podIP)
+	commands := []string{"curl", "-u", fmt.Sprintf("operator:%v", pass), httpURL}
 
 	logger := log.WithValues("Request.Namespace", namespace, "Secret.Name", secretName, "Pod.Name", podName)
-	logger.Info("get cluster members", "url", httpUrl)
+	logger.Info("get cluster members", "url", httpURL)
 
 	execOptions := ExecOptions{Command: commands, PodName: podName, Namespace: namespace}
 	execOut, execErr, err := ExecWithOptions(execOptions)
--- pkg/controller/infinispan/util/configuration.go
+++ pkg/controller/infinispan/util/configuration.go
@@ -5,23 +5,27 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// Infinispan is the top level configuration type
 type Infinispan struct {
 	ClusterName string `yaml:"clusterName"`
 	JGroups     JGroups
 }
 
+// JGroups configures clustering layer
 type JGroups struct {
 	Transport string
-	DnsPing   DnsPing `yaml:"dnsPing"`
+	DNSPing   DNSPing `yaml:"dnsPing"`
 }
 
-type DnsPing struct {
+// DNSPing configures DNS cluster lookup settings
+type DNSPing struct {
 	Query string
 }
 
+// InfinispanConfiguration generates a server configuration
 func InfinispanConfiguration(name, namespace string) (string, error) {
 	query := fmt.Sprintf("%s-ping.%s.svc.cluster.local", name, namespace)
-	jgroups := JGroups{Transport: "tcp", DnsPing: DnsPing{Query: query}}
+	jgroups := JGroups{Transport: "tcp", DNSPing: DNSPing{Query: query}}
 	config := Infinispan{ClusterName: name, JGroups: jgroups}
 	serialized, err := yaml.Marshal(&config)
 	if err != nil {
