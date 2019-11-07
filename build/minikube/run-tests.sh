5657b86b90b2e8196f01a5186bd6cd428e26708b
Galder Zamarreño <galder@zamarreno.com>
2019-11-07 14:05:08 +0100

    Set CPU/Memory resource limits for server #205
    * Set limits on Infinispan Server pod resources.
    * Limits are required for the cache service,
    since it'll decide on the default cache settings based on those limits.

--- build/minikube/run-tests.sh
+++ build/minikube/run-tests.sh
@@ -4,4 +4,4 @@ NAMESPACE=${1}
 
 echo "Using KUBECONFIG '$KUBECONFIG'"
 
-KUBECONFIG=${2-${HOME}/.kube/config} GOCACHE=off go test -v ./test/e2e
+KUBECONFIG=${2-${HOME}/.kube/config} GOCACHE=off go test -v ./test/e2e -timeout 15m
