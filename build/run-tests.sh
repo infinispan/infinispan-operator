6e79e1b64432754e695718e10e1ffdbd9a975e1f
Galder Zamarreño <galder@zamarreno.com>
2019-11-11 12:41:08 +0100

    Upgrade to apps/v1/StatefulSet and enable GOCACHE when testing
    * Both are required to be able to build with Go 1.13.

--- build/run-tests.sh
+++ build/run-tests.sh
@@ -4,4 +4,5 @@ KUBECONFIG=${1-openshift.local.clusterup/openshift-apiserver/admin.kubeconfig}
 
 echo "Using KUBECONFIG '$KUBECONFIG'"
 
-GOCACHE=off go test -timeout 15m -v ./test/e2e -timeout 15m
+go clean -testcache
+go test -v ./test/e2e -timeout 15m
