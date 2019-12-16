f2a5197bd2fae96e3f2b5c29212b80891744895e
Dmitry Volodin <dmvolod@gmail.com>
2019-12-16 15:39:45 +0300

    Move build from the 'dep' to 'mod' golang dependencies manager Fixes #234

--- build/build.sh
+++ build/build.sh
@@ -6,11 +6,9 @@ set -o pipefail
 
 GOOS=${1-linux}
 BIN_DIR=${2-$(pwd)/build/_output/bin}
-mkdir -p ${BIN_DIR}
 PROJECT_NAME="infinispan-operator"
-REPO_PATH="github.com/infinispan/infinispan-operator"
-BUILD_PATH="${REPO_PATH}/cmd/manager"
+BUILD_PATH="./cmd/manager"
 VERSION="$(git describe --tags --always --dirty)"
-GO_LDFLAGS="-X ${REPO_PATH}/version.Version=${VERSION}"
+GO_LDFLAGS="-X ./version.Version=${VERSION}"
 echo "building ${PROJECT_NAME}..."
 GOOS=${GOOS} GOARCH=amd64 CGO_ENABLED=0 go build -o ${BIN_DIR}/${PROJECT_NAME} -ldflags "${GO_LDFLAGS}" $BUILD_PATH
