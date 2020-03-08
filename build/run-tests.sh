2d640a826db1a5ab5d79645f901e792a43532d63
Dmitry Volodin <dmvolod@gmail.com>
2020-03-08 09:19:19 +0300

    Check err and validate that issue #312 runs on k8s Fixes #312

--- build/run-tests.sh
+++ build/run-tests.sh
@@ -4,5 +4,8 @@ KUBECONFIG=${1-openshift.local.clusterup/openshift-apiserver/admin.kubeconfig}
 
 echo "Using KUBECONFIG '$KUBECONFIG'"
 
+VERSION=$(git describe --tags --always --dirty)
+GO_LDFLAGS="-X github.com/infinispan/infinispan-operator/version.Version=${VERSION}"
+
 go clean -testcache ./test/e2e
-go test -v ./test/e2e -timeout 45m
+go test -v ./test/e2e -timeout 45m -ldflags "${GO_LDFLAGS}"
