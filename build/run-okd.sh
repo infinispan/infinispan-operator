c9b5606d7639da405c8c71d082c60b0b43a061f6
Ryan Emerson <remerson@redhat.com>
2020-03-17 10:17:00 +0000

    Parameterize oc login

--- build/run-okd.sh
+++ build/run-okd.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+OC_USER=${OC_USER:-kubeadmin}
 KUBECONFIG=${1-openshift.local.clusterup/openshift-apiserver/admin.kubeconfig}
 
 echo "Using KUBECONFIG '$KUBECONFIG'"
 
-oc login -u system:admin
+oc login -u ${USER}
 oc project default
 oc apply -f deploy/rbac.yaml
 oc apply -f deploy/operator.yaml
