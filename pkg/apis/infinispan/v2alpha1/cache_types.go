6d8f758a41ac0c87957519bcf0fb1bf70a1f4975
Vittorio Rigamonti <vrigamon@redhat.com>
2020-03-23 17:39:19 +0100

    operator sdk add cache

---
+++ pkg/apis/infinispan/v2alpha1/cache_types.go
@@ -0,0 +1,48 @@
+package v2alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CacheSpec defines the desired state of Cache
+type CacheSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// CacheStatus defines the observed state of Cache
+type CacheStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Cache is the Schema for the caches API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=caches,scope=Namespaced
+type Cache struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CacheSpec   `json:"spec,omitempty"`
+	Status CacheStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CacheList contains a list of Cache
+type CacheList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Cache `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Cache{}, &CacheList{})
+}
---
+++ pkg/apis/infinispan/v2alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v2alpha1 contains API Schema definitions for the infinispan v2alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=infinispan.org
+package v2alpha1
