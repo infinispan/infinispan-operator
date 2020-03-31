b103cbb118f9c4796b53f1d62896154e6df1578c
Dmitry Volodin <dmvolod@gmail.com>
2020-03-31 21:01:17 +0300

    Move constants in test

---
+++ test/e2e/constants/constants.go
@@ -0,0 +1,43 @@
+package constants
+
+import (
+	"strings"
+	"time"
+
+	comutil "github.com/infinispan/infinispan-operator/pkg/controller/utils/common"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	TestTimeout      = 5 * time.Minute
+	SinglePodTimeout = 5 * time.Minute
+	RouteTimeout     = 240 * time.Second
+	// Default retry time when waiting for resources
+	DefaultPollPeriod = 1 * time.Second
+	// Maximum time to wait for resources
+	MaxWaitTimeout      = 120 * time.Second
+	DefaultClusterPort  = 11222
+	DefaultClusterName  = "test-node-startup"
+	ClusterUpKubeConfig = "../../openshift.local.clusterup/kube-apiserver/admin.kubeconfig"
+)
+
+var (
+	CPU               = comutil.GetEnvWithDefault("INFINISPAN_CPU", "500m")
+	Memory            = comutil.GetEnvWithDefault("INFINISPAN_MEMORY", "512Mi")
+	Namespace         = strings.ToLower(comutil.GetEnvWithDefault("TESTING_NAMESPACE", "namespace-for-testing"))
+	RunLocalOperator  = comutil.GetEnvWithDefault("RUN_LOCAL_OPERATOR", "true")
+	ImageName         = comutil.GetEnvWithDefault("IMAGE", "registry.hub.docker.com/infinispan/server")
+	ExposeServiceType = comutil.GetEnvWithDefault("EXPOSE_SERVICE_TYPE", "NodePort")
+)
+
+// Options used when deleting resources
+var DeleteOpts = []client.DeleteOption{
+	client.GracePeriodSeconds(int64(0)),
+	client.PropagationPolicy(metav1.DeletePropagationBackground),
+}
+
+var InfinispanTypeMeta = metav1.TypeMeta{
+	APIVersion: "infinispan.org/v1",
+	Kind:       "Infinispan",
+}
