ac4320283d588fa4b779b5cb25ab7436da2deb6e
Dmitry Volodin <dmvolod@gmail.com>
2020-03-31 19:20:22 +0300

    Move constants and common functions

---
+++ pkg/controller/utils/common/labels.go
@@ -0,0 +1,10 @@
+package common
+
+// LabelsResource returns the labels that must me applied to the resource
+func LabelsResource(name, resourceType string) map[string]string {
+	m := map[string]string{"infinispan_cr": name, "clusterName": name}
+	if resourceType != "" {
+		m["app"] = resourceType
+	}
+	return m
+}
---
+++ pkg/controller/utils/common/pods.go
@@ -0,0 +1,46 @@
+package common
+
+import corev1 "k8s.io/api/core/v1"
+
+// GetPodDefaultImage returns an Infinispan pod's default image.
+// If the default image cannot be found, it returns the running image.
+func GetPodDefaultImage(container corev1.Container) string {
+	envs := container.Env
+	for _, env := range envs {
+		if env.Name == "DEFAULT_IMAGE" {
+			return env.Value
+		}
+	}
+
+	return container.Image
+}
+
+func AreAllPodsReady(podList *corev1.PodList) bool {
+	for _, pod := range podList.Items {
+		containerStatuses := pod.Status.ContainerStatuses
+		if len(containerStatuses) == 0 || !containerStatuses[0].Ready {
+			return false
+		}
+	}
+
+	return true
+}
+
+func ArePodIPsReady(pods *corev1.PodList) bool {
+	for _, pod := range pods.Items {
+		if pod.Status.PodIP == "" {
+			return false
+		}
+	}
+
+	return true
+}
+
+func IsPodReady(pod corev1.Pod) bool {
+	for _, cond := range pod.Status.Conditions {
+		if cond.Type == corev1.ContainersReady && cond.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+	return false
+}
