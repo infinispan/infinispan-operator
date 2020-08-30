cbb2124977ac399f6995c58da0c1185eeb31c25d
Dmitry Volodin <dmvolod@gmail.com>
2020-08-30 21:27:38 +0300

    Unable to delete Infinispan after cluster upgrade from 1.1.x to 2.0.x

--- pkg/controller/infinispan/autoscale.go
+++ pkg/controller/infinispan/autoscale.go
@@ -78,7 +78,10 @@ func autoscalerLoop(clusterNsn types.NamespacedName, r *ReconcileInfinispan) {
 		// Data memory percent usage array, one value per pod
 		metricDataMemoryPercentUsed := map[string]int{}
 		podList := &corev1.PodList{}
-		kubernetes.GetK8sResources(ispn.ObjectMeta.Name, ispn.ObjectMeta.Namespace, podList)
+		err = kubernetes.ResourcesList(ispn.ObjectMeta.Name, ispn.ObjectMeta.Namespace, "infinispan-pod", podList)
+		if err != nil {
+			continue
+		}
 
 		for _, pItem := range podList.Items {
 			// time.Sleep(time.Duration(10000/len(podList.Items)) * time.Millisecond)
