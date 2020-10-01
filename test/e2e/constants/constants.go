f3e9a8570a6253ba935dad4fab72c108df47d85c
Dmitry Volodin <dmvolod@gmail.com>
2020-10-01 11:48:42 +0300

    #592 Namespace should be created if not found in test

--- test/e2e/constants/constants.go
+++ test/e2e/constants/constants.go
@@ -26,7 +26,7 @@ var (
 	Memory            = constants.GetEnvWithDefault("INFINISPAN_MEMORY", "512Mi")
 	Namespace         = strings.ToLower(constants.GetEnvWithDefault("TESTING_NAMESPACE", "namespace-for-testing"))
 	RunLocalOperator  = strings.ToUpper(constants.GetEnvWithDefault("RUN_LOCAL_OPERATOR", "true"))
-	RunSaOperator     = strings.ToUpper(constants.GetEnvWithDefault("RUN_SA_OPERATOR", "true"))
+	RunSaOperator     = strings.ToUpper(constants.GetEnvWithDefault("RUN_SA_OPERATOR", "false"))
 	ImageName         = constants.GetEnvWithDefault("IMAGE", "infinispan/server:12.0")
 	ExposeServiceType = constants.GetEnvWithDefault("EXPOSE_SERVICE_TYPE", "NodePort")
 )
