5a1d61d3f9fa32e4591c3f0fb8d22cd787333277
William Burns <mudokonman@gmail.com>
2021-02-11 10:58:15 -0500

    adding in controller reference

--- pkg/infinispan/grafana/dashboard_controller.go
+++ pkg/infinispan/grafana/dashboard_controller.go
@@ -4,8 +4,10 @@ import (
 	"context"
 
 	"github.com/infinispan/infinispan-operator/pkg/controller/constants"
+	"github.com/infinispan/infinispan-operator/pkg/controller/infinispan"
 	grafanav1alpha1 "github.com/integr8ly/grafana-operator/v3/pkg/apis/integreatly/v1alpha1"
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
+	appsv1 "k8s.io/api/apps/v1"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/discovery"
@@ -41,7 +43,27 @@ func Create(namespace string, mgr manager.Manager) error {
 
 	infinispanDashboard := dashboard(namespace)
 
+	operatorDeployment := &appsv1.Deployment{}
+	result, err := infinispan.LookupResource("infinispan-operator", namespace, operatorDeployment, controllerClient, log)
+
+	if err != nil {
+		log.Error(err, "There was a problem looking up the infinispan operator to add the dashboard as a secondary resource")
+		return err
+	}
+
+	if result != nil {
+		log.Info("Infinispan operator resource not initialized, not installing Grafana Dashboard")
+		return nil
+	}
+
+	// result and err should be analyzed
+	if err = controllerutil.SetControllerReference(operatorDeployment, infinispanDashboard, mgr.GetScheme()); err != nil {
+		log.Error(err, "Unable to set Grafana Dashboard controller reference to Infinispan operator - skipping creation")
+		return nil
+	}
+
 	if _, err = controllerutil.CreateOrUpdate(context.Background(), controllerClient, infinispanDashboard, func() error {
+		// Don't do any merge
 		return nil
 	}); err != nil {
 		return err
