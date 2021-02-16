c6638fd65f68e195f00b9d9296165ee937121f1a
Vittorio Rigamonti <vrigamon@redhat.com>
2021-02-16 16:40:20 +0100

    fix

--- test/e2e/main/main_test.go
+++ test/e2e/main/main_test.go
@@ -132,7 +132,7 @@ func TestNodeStartup(t *testing.T) {
 			operatorNS = spec.Namespace
 		}
 		// operator deployed on cluster, labels are set by the deployment
-		if !areOperatorLabelsPropagated(spec.Namespace, ispnv1.OperatorPodTargetLabelsEnvVarName, pod.Labels) {
+		if !areOperatorLabelsPropagated(operatorNS, ispnv1.OperatorPodTargetLabelsEnvVarName, pod.Labels) {
 			panic("Operator labels haven't been propagated to pods")
 		}
 	}
@@ -164,7 +164,7 @@ func TestNodeStartup(t *testing.T) {
 				operatorNS = spec.Namespace
 			}
 			// operator deployed on cluster, labels are set by the deployment
-			if !areOperatorLabelsPropagated(spec.Namespace, ispnv1.OperatorTargetLabelsEnvVarName, svc.Labels) {
+			if !areOperatorLabelsPropagated(operatorNS, ispnv1.OperatorTargetLabelsEnvVarName, svc.Labels) {
 				panic("Operator labels haven't been propagated to services")
 			}
 		}
