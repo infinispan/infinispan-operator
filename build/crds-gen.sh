c2af53c63e9390c86b8c3740746f8774782d2c8d
Dmitry Volodin <dmvolod@gmail.com>
2021-03-24 09:40:24 +0300

    Rootless Operator SDK installation #925

--- build/crds-gen.sh
+++ build/crds-gen.sh
@@ -3,69 +3,70 @@
 . $(dirname "$0")/common.sh
 
 OPERATOR_SDK_VERSION=${1}
-ARC=$(echo $MACHTYPE | grep -oE '^[a-z0-9_]+')
-OS=$(echo $OSTYPE | sed 's/darwin/apple-darwin/g' | grep -oE '[a-z-]+')
+YQ_VERSION=${2}
+
+ARC=$(echo "${MACHTYPE}" | grep -oE '^[a-z0-9_]+')
+OS=$(echo "${OSTYPE}" | sed 's/darwin/apple-darwin/g' | grep -oE '[a-z-]+')
 OPERATOR_SDK_BUNDLE=operator-sdk-${OPERATOR_SDK_VERSION}-${ARC}-${OS}
 OPERATOR_SDK_URL=https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_VERSION}/${OPERATOR_SDK_BUNDLE}
 
-unset GOPATH
 GO111MODULE=on
 # shellcheck disable=SC2155
 export GOROOT=$(go env GOROOT)
+OPERATOR_SDK_INSTALL_PATH=$(go env GOPATH)/bin
 
 installOperatorSDK() {
-  validateROOT
   printf "Installing Operator SDK version %s ..." "${OPERATOR_SDK_VERSION}"
-  curl -LOs "${OPERATOR_SDK_URL}"
-  if grep -Fxq "Not Found" "${OPERATOR_SDK_BUNDLE}" &> /dev/null; then
+  if ! curl -LOsf "${OPERATOR_SDK_URL}"; then
     printf "Incorrect version\n"
-    rm -rf "${OPERATOR_SDK_BUNDLE}"
     exit 1
   fi
-  chmod +x "${OPERATOR_SDK_BUNDLE}" && sudo mkdir -p /usr/local/bin/ && sudo cp "${OPERATOR_SDK_BUNDLE}" /usr/local/bin/operator-sdk && rm "${OPERATOR_SDK_BUNDLE}"
+
+  chmod +x "${OPERATOR_SDK_BUNDLE}" && mv "${OPERATOR_SDK_BUNDLE}" "${OPERATOR_SDK_INSTALL_PATH}"/operator-sdk
   printf "Installed\n"
 }
 
-printf "Validating Operator SDK installation..."
+validateOperatorSDK() {
+  printf "Validating Operator SDK installation..."
 
-if ! [ -x "$(command -v operator-sdk)" ]; then
-  printf "Not found\n"
-  installOperatorSDK
-else
-  printf "OK\n"
-  printf "Validating Operator SDK version..."
-  INSTALLED_OPERATOR_SDK_VERSION=$(operator-sdk version | grep -oE 'operator-sdk version: "[v0-9\.]+"' | grep -oE '[^"][0-9\.]+')
-  if [ "${INSTALLED_OPERATOR_SDK_VERSION}" == "${OPERATOR_SDK_VERSION}" ]; then
-    printf "%s\n" "${OPERATOR_SDK_VERSION}"
-  else
-    printf "Incorrect version %s\n" "${INSTALLED_OPERATOR_SDK_VERSION}"
+  if ! [ -x "$(command -v operator-sdk)" ]; then
+    printf "Not found\n"
     installOperatorSDK
-  fi
-fi
-
-validsed() {
-  if [[ "$OS" == "apple-darwin" ]]; then
-    if [[ -z $(which gsed) ]]; then
-      echo "gnu-sed not found, please install with:"
-      echo "$ brew install gnu-sed"
-      exit 1
+  else
+    printf "OK\n"
+    printf "Validating Operator SDK version..."
+    INSTALLED_OPERATOR_SDK_VERSION=$(operator-sdk version | grep -oE 'operator-sdk version: "[v0-9\.]+"' | grep -oE '[^"][0-9\.]+')
+    if [ "${INSTALLED_OPERATOR_SDK_VERSION}" == "${OPERATOR_SDK_VERSION}" ]; then
+      printf "%s\n" "${OPERATOR_SDK_VERSION}"
+    else
+      printf "Incorrect version %s\n" "${INSTALLED_OPERATOR_SDK_VERSION}"
+      installOperatorSDK
     fi
   fi
 }
 
-allsed() {
-  validsed
-  if [[ "$OS" == "apple-darwin" ]]; then
-    gsed "$@" # for mac, install with brew install gnu-sed
-  else
-    sed "$@"
+if ! validateYQ; then
+  printf "Valid yq not found in PATH. Trying adding GOPATH/bin\n"
+  PATH=$(go env GOPATH)/bin:$PATH
+  if ! validateYQ; then
+    installYQ
   fi
-}
+fi
 
-validsed
+if ! validateOperatorSDK; then
+  printf "Valid Operator SDK not found in PATH. Trying adding GOPATH/bin\n"
+  PATH=$(go env GOPATH)/bin:$PATH
+  if ! validateOperatorSDK; then
+    installOperatorSDK
+  fi
+fi
 
 echo "Generating CRDs for API's..."
 if operator-sdk generate crds; then
+  for CRD_FILE in deploy/crds/infinispan.org_*_crd.yaml; do
+    yq ea -ie '.metadata.labels={"name":"infinispan-operator"}' "${CRD_FILE}"
+  done
+
   echo "Generating Kubernetes code for custom resources..."
   operator-sdk generate k8s
 fi
--- build/update-csv.sh
+++ build/update-csv.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-YQ_MAJ_VERSION=${1:-4}
-installYQ() {
-    printf "Installing yq ..."
-    GO111MODULE=off go get -u github.com/myitcv/gobin
-    gobin github.com/mikefarah/yq/v4@v4.6.1
-    yq --version
-}
+. $(dirname "$0")/common.sh
+
+YQ_VERSION=${1-4.6.1}
 
 OPERATOR_ROLES_FILES=(
     "deploy/role.yaml"
@@ -14,25 +10,6 @@ OPERATOR_ROLES_FILES=(
 )
 
 OPERATOR_CSV_FILE="deploy/olm-catalog/infinispan-operator.clusterserviceversion.yaml"
-
-validateYQ() {
- printf "Validating yq installation..."
-if ! [ -x "$(command -v yq)" ]; then
-  printf "Not found\n"
-  return 1
-else
-  printf "OK\n"
-  printf "Validating yq major version..."
-  INSTALLED_YQ_MAJ_VERSION=$(yq --version | grep -oE 'yq version [0-9]+' | grep -oE '[0-9]+')
-  if [ "${INSTALLED_YQ_MAJ_VERSION}" == "${YQ_MAJ_VERSION}" ]; then
-    printf "%s\n" "${YQ_MAJ_VERSION}"
-  else
-    printf "incorrect major version %s\n" "${INSTALLED_YQ_MAJ_VERSION}"
-    return 1
-  fi
-fi
- 
-}
 
 #Ensure that GOPATH/bin is in the PATH
 if ! validateYQ; then
@@ -43,4 +20,4 @@ if ! validateYQ; then
   fi
 fi
 
-yq ea -i 'select(fileIndex == 0).spec.install.spec.permissions[0].rules=select(fileIndex == 1).rules|select(fileIndex == 0).spec.install.spec.clusterPermissions[0].rules=select(fileIndex == 2).rules|select(fileIndex==0)' ${OPERATOR_CSV_FILE} ${OPERATOR_ROLES_FILES[@]}
+yq ea -i 'select(fileIndex == 0).spec.install.spec.permissions[0].rules=select(fileIndex == 1).rules|select(fileIndex == 0).spec.install.spec.clusterPermissions[0].rules=select(fileIndex == 2).rules|select(fileIndex==0)' "${OPERATOR_CSV_FILE}" "${OPERATOR_ROLES_FILES[@]}"
