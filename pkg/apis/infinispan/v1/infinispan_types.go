3cc98e5def377fef5fce35d101d73a9faa9797f7
Vittorio Rigamonti <vrigamon@redhat.com>
2021-04-07 15:25:33 +0200

    fix

--- pkg/apis/infinispan/v1/infinispan_types.go
+++ pkg/apis/infinispan/v1/infinispan_types.go
@@ -231,8 +231,6 @@ type InfinispanCloudEvents struct {
 
 // InfinispanSpec defines the desired state of Infinispan
 type InfinispanSpec struct {
-<<<<<<< HEAD
-<<<<<<< HEAD
 	Replicas int32 `json:"replicas"`
 	// +optional
 	Image *string `json:"image,omitempty"`
@@ -251,7 +249,7 @@ type InfinispanSpec struct {
 	// +optional
 	Affinity *corev1.Affinity `json:"affinity,omitempty"`
 	// +optional
-	CloudEvents *InfinispanCloudEvents  `json:"cloudEvents,omitempty"`
+	CloudEvents *InfinispanCloudEvents `json:"cloudEvents,omitempty"`
 	// External dependencies needed by the Infinispan cluster
 	// +optional
 	Dependencies *InfinispanExternalDependencies `json:"dependencies,omitempty"`
