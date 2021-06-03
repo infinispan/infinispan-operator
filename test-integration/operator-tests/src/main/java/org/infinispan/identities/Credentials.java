2629b62a125eb370abaa1d19ce7ffc3e92ab7574
Pavel Drobek <pdrobek@redhat.com>
2021-06-03 12:11:06 +0200

    [test-integration] Small fixes

--- test-integration/operator-tests/src/main/java/org/infinispan/identities/Credentials.java
+++ test-integration/operator-tests/src/main/java/org/infinispan/identities/Credentials.java
@@ -3,9 +3,12 @@ package org.infinispan.identities;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class Credentials {
    private String username;
    private String password;
+   private List<String> roles;
 }
