430d5765439190c915be5f0b42035b2a55cbe164
Dmitry Volodin <dmvolod@gmail.com>
2021-06-10 12:05:56 +0300

    Move Kubernetes objects for testing outside the operator kubernetes package fixes #1112

--- build/run-local.sh
+++ build/run-local.sh
@@ -11,4 +11,4 @@ oc login -u "${OC_USER}"
 oc project "${PROJECT_NAME}"
 ./build/install-crds.sh
 oc wait --for condition=established crd infinispans.infinispan.org --timeout=60s
-WATCH_NAMESPACE="${PROJECT_NAME}" OSDK_FORCE_RUN_MODE=local ./build/_output/bin/infinispan-operator -kubeconfig "${KUBECONFIG}"
+WATCH_NAMESPACE="${PROJECT_NAME}" OSDK_FORCE_RUN_MODE=local OPERATOR_NAME=infinispan-operator ./build/_output/bin/infinispan-operator -kubeconfig "${KUBECONFIG}"
