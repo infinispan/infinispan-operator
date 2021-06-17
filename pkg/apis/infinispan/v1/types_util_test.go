38ade47c1553f37c8fa6051feb7a975dd7cdebf3
Vittorio Rigamonti <vrigamon@redhat.com>
2021-06-17 16:32:37 +0200

    Sort label by key #1142

--- pkg/apis/infinispan/v1/types_util_test.go
+++ pkg/apis/infinispan/v1/types_util_test.go
@@ -1,6 +1,9 @@
 package v1
 
 import (
+	"encoding/json"
+	"os"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -27,4 +30,45 @@ func TestServiceExternalName(t *testing.T) {
 
 	exposeRouteInfinispan.Name = "example-infinispan"
 	assert.Equal(t, "example-infinispan-external", exposeRouteInfinispan.GetServiceExternalName(), "Route expose name")
+}
+
+func TestApplyOperatorLabels(t *testing.T) {
+	testTable := []struct {
+		Labels            string
+		PodLabels         string
+		ExpectedLabels    string
+		ExpectedPodLabels string
+	}{
+		{"", "", "", ""},
+		{"{\"label\":\"value\"}", "", "label", ""},
+		{"", "{\"label\":\"value\"}", "", "label"},
+		{"{\"l2\":\"v2\",\"l1\":\"v1\"}", "", "l1,l2", ""},
+		{"", "{\"l2\":\"v2\",\"l1\":\"v1\"}", "", "l1,l2"},
+		{"{\"l2\":\"v2\",\"l1\":\"v1\"}", "{\"lp2\":\"v2\",\"lp1\":\"v1\"}", "l1,l2", "lp1,lp2"},
+	}
+	for _, testItem := range testTable {
+		err := os.Setenv("INFINISPAN_OPERATOR_TARGET_LABELS", testItem.Labels)
+		assert.Nil(t, err)
+		err = os.Setenv("INFINISPAN_OPERATOR_POD_TARGET_LABELS", testItem.PodLabels)
+		assert.Nil(t, err)
+		ispn := exposeRouteInfinispan.DeepCopy()
+		err = ispn.ApplyOperatorLabels()
+		assert.Nil(t, err)
+		assert.Equal(t, ispn.Annotations[OperatorTargetLabels], testItem.ExpectedLabels)
+		assert.Equal(t, ispn.Annotations[OperatorPodTargetLabels], testItem.ExpectedPodLabels)
+		labelMap := make(map[string]string)
+		if testItem.Labels != "" {
+			err = json.Unmarshal([]byte(testItem.Labels), &labelMap)
+			assert.Nil(t, err)
+		}
+		labelPodMap := make(map[string]string)
+		if testItem.PodLabels != "" {
+			err = json.Unmarshal([]byte(testItem.PodLabels), &labelPodMap)
+			assert.Nil(t, err)
+		}
+		for n, v := range labelMap {
+			labelPodMap[n] = v
+		}
+		assert.True(t, reflect.DeepEqual(ispn.Labels, labelPodMap) || len(labelPodMap) == 0 && ispn.Labels == nil)
+	}
 }
