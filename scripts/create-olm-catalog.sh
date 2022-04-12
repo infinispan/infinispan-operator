680c887cf9c94a444a93a0489c469d2dc56e0edd
Ryan Emerson <remerson@redhat.com>
2022-04-12 15:15:13 +0100

    Create OLM catalog declaratively
    - Allows greater flexibility over the exact OLM upgrade graph when
      testing locally
    - Removes need for slow call to `opm render` for the operatorhub catalog
      image

--- scripts/create-olm-catalog.sh
+++ scripts/create-olm-catalog.sh
@@ -3,27 +3,41 @@ set -e
 
 CATALOG_DIR=infinispan-olm-catalog
 DOCKERFILE=${CATALOG_DIR}.Dockerfile
-CATALOG=${CATALOG_DIR}/catalog.json
-BUNDLE=${CATALOG_DIR}/bundle.json
+CATALOG=${CATALOG_DIR}/catalog.yaml
 
-mkdir -p ${CATALOG_DIR}
+BUNDLE_IMGS="${BUNDLE_IMG}"
+# Define existing bundle images required in the catalog
+for version in v2.2.1 v2.2.2 v2.2.3 v2.2.4; do
+  BUNDLE_IMGS="${BUNDLE_IMGS} quay.io/operatorhubio/infinispan:$version"
+done
 
-${OPM} render --use-http ${CATALOG_BASE_IMG} > ${CATALOG}
-${OPM} render --use-http ${BUNDLE_IMGS} > ${BUNDLE}
+rm -rf ${CATALOG_DIR}
+mkdir ${CATALOG_DIR}
 
-default_channel=$(${JQ} 'select((.name=="infinispan") and (.schema=="olm.package"))' ${CATALOG} | ${JQ} -r .defaultChannel)
-channel_selector="(.package==\"infinispan\") and (.schema==\"olm.channel\") and (.name==\"${default_channel}\")"
-old_channel=$(${JQ} -r "select(${channel_selector})" ${CATALOG})
-old_channel_latest_bundle=$(echo $old_channel | ${JQ} -r '.entries[-1].name')
+# Define OLM update graph
+cat <<EOF >> ${CATALOG}
+---
+schema: olm.package
+name: infinispan
+defaultChannel: 2.2.x
+---
+schema: olm.channel
+name: 2.2.x
+package: infinispan
+entries:
+- name: infinispan-operator.v2.2.1
+  replaces: infinispan-operator.v2.2.0
+- name: infinispan-operator.v2.2.2
+  replaces: infinispan-operator.v2.2.1
+- name: infinispan-operator.v2.2.3
+  replaces: infinispan-operator.v2.2.2
+- name: infinispan-operator.v2.2.4
+  replaces: infinispan-operator.v2.2.3
+- name: infinispan-operator.v2.2.5
+  replaces: infinispan-operator.v2.2.4
+EOF
 
-bundle_name=$(${JQ} -r .name ${BUNDLE})
-new_channel=$(echo $old_channel | ${JQ} ".entries += [{\"name\":\"${bundle_name}\", \"replaces\":\"${old_channel_latest_bundle}\"}]")
-
-new_catalog=$(${JQ} -r "select(${channel_selector} | not)" ${CATALOG})
-echo ${new_channel} ${new_catalog} > ${CATALOG_DIR}/new_catalog.json
-
-# Remove the original catalog no longer required
-rm -f ${CATALOG}
+${OPM} render --use-http -o yaml ${BUNDLE_IMGS} >> ${CATALOG}
 
 ${OPM} validate ${CATALOG_DIR}
 ${OPM} generate dockerfile ${CATALOG_DIR}
