eba0514202f164745c37c28b320c3556377976f0
Ryan Emerson <remerson@redhat.com>
2022-07-26 11:43:30 +0100

    Convert HR rolling upgrade test to use OLM upgrades

--- test/e2e/hotrod-rolling-upgrade/hotrod_rolling_upgrade_test.go
+++ test/e2e/hotrod-rolling-upgrade/hotrod_rolling_upgrade_test.go
@@ -1,24 +1,20 @@
 package hotrod_rolling_upgrade
 
 import (
-	"bufio"
-	"context"
 	"fmt"
 	"os"
-	"os/exec"
 	"strconv"
-	"syscall"
 	"testing"
 
 	ispnv1 "github.com/infinispan/infinispan-operator/api/v1"
 	"github.com/infinispan/infinispan-operator/pkg/mime"
 	tutils "github.com/infinispan/infinispan-operator/test/e2e/utils"
+	coreos "github.com/operator-framework/api/pkg/operators/v1alpha1"
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
-	mainPath        = "../../../main.go"
-	crdPath         = "../../../config/crd/bases/"
 	entriesPerCache = 100
 	numPods         = 2
 	clusterName     = "demo-cluster"
@@ -26,73 +22,39 @@ const (
 
 var testKube = tutils.NewTestKubernetes(os.Getenv("TESTING_CONTEXT"))
 
-// command Pointer to the spawned operator process
-var command *exec.Cmd
-
-// initialize Prepares namespaces and CRD installations
-func initialize() {
-	testKube.NewNamespace(tutils.Namespace)
-}
-
-// killOperator Gracefully shutdown the operator and its subprocesses
-func killOperator() {
-	err := syscall.Kill(-command.Process.Pid, syscall.SIGTERM)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func runOperatorSameProcess() context.CancelFunc {
-	return testKube.RunOperator(tutils.Namespace, crdPath)
-}
-
-// runOperatorNewProcess Starts the operator in a new Process, as it's not possible to start it after stopping in the
-//same process due to several internal concurrency issues like https://github.com/kubernetes-sigs/controller-runtime/issues/1346
-func runOperatorNewProcess(image string) {
-	command = exec.Command("go", "run", mainPath, "operator")
-
-	// Sets the gid since "go run" spawns a subprocess
-	command.SysProcAttr = &syscall.SysProcAttr{
-		Setpgid: true,
-	}
-	command.Env = os.Environ()
-	command.Env = append(command.Env, "RELATED_IMAGE_OPENJDK="+image)
-	command.Env = append(command.Env, "OSDK_FORCE_RUN_MODE=local")
-	command.Env = append(command.Env, "WATCH_NAMESPACE="+tutils.Namespace)
-	command.Env = append(command.Env, "ENABLE_WEBHOOKS=false")
-
-	stdoutPipe, _ := command.StdoutPipe()
-	command.Stderr = command.Stdout
-	ch := make(chan struct{})
-	scanner := bufio.NewScanner(stdoutPipe)
-	go func() {
-		for scanner.Scan() {
-			line := scanner.Text()
-			fmt.Println(line)
-		}
-		ch <- struct{}{}
-	}()
-	if err := command.Start(); err != nil {
-		panic(err)
-	}
-	<-ch
-	if err := command.Wait(); err != nil {
-		println("Operator stopped")
-	}
-	<-ch
-}
-
 func TestMain(t *testing.M) {
 	code := t.Run()
 	os.Exit(code)
 }
 
 func TestRollingUpgrade(t *testing.T) {
-	defer testKube.CleanNamespaceAndLogOnPanic(t, tutils.Namespace)
+	olm := testKube.OLMTestEnv()
+	olm.PrintManifest()
+	sourceChannel := olm.SourceChannel
+	targetChannel := olm.TargetChannel
 
-	initialize()
-	// TODO This should always trigger a rolling upgrade since the operator uses aliases by default. Make it an environment variable to test upgrade from different versions
-	go runOperatorNewProcess("quay.io/infinispan/server")
+	testKube.NewNamespace(tutils.Namespace)
+	sub := &coreos.Subscription{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: coreos.SubscriptionCRDAPIVersion,
+			Kind:       coreos.SubscriptionKind,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      olm.SubName,
+			Namespace: olm.SubNamespace,
+		},
+		Spec: &coreos.SubscriptionSpec{
+			Channel:                olm.SourceChannel.Name,
+			CatalogSource:          olm.CatalogSource,
+			CatalogSourceNamespace: olm.CatalogSourceNamespace,
+			InstallPlanApproval:    coreos.ApprovalManual,
+			Package:                olm.SubPackage,
+			StartingCSV:            olm.SubStartingCSV,
+		},
+	}
+
+	defer testKube.CleanupOLMTest(t, olm.SubName, olm.SubNamespace, olm.SubPackage)
+	testKube.CreateSubscriptionAndApproveInitialVersion(olm, sub)
 
 	infinispan := tutils.DefaultSpec(t, testKube, func(i *ispnv1.Infinispan) {
 		i.Name = clusterName
@@ -123,30 +85,40 @@ func TestRollingUpgrade(t *testing.T) {
 	addData("textCache", entriesPerCache, client)
 	addData("indexedCache", entriesPerCache, client)
 
-	// Kill the operator
-	killOperator()
-
-	// Restart the operator
-	runOperatorSameProcess()
-
-	// Check migration
-	currentStatefulSetName := clusterName
-	newStatefulSetName := clusterName + "-1"
-
-	testKube.WaitForStateFulSet(newStatefulSetName, tutils.Namespace)
-	testKube.WaitForStateFulSetRemoval(currentStatefulSetName, tutils.Namespace)
-	testKube.WaitForInfinispanPodsCreatedBy(numPods, tutils.SinglePodTimeout, newStatefulSetName, tutils.Namespace)
-	testKube.WaitForInfinispanPodsCreatedBy(0, tutils.SinglePodTimeout, currentStatefulSetName, tutils.Namespace)
-
-	if !tutils.CheckExternalAddress(client) {
-		panic("Error contacting server")
+	// Upgrade the Subscription channel if required
+	if sourceChannel != targetChannel {
+		testKube.UpdateSubscriptionChannel(targetChannel.Name, sub)
 	}
 
-	// Check data
-	assert.Equal(t, entriesPerCache, cacheSize("textCache", client))
-	assert.Equal(t, entriesPerCache, cacheSize("indexedCache", client))
-	assert.Equal(t, 0, cacheSize("jsonCache", client))
-	assert.Equal(t, 0, cacheSize("javaCache", client))
+	// Approve InstallPlans and verify cluster state on each upgrade until the most recent CSV has been reached
+	for testKube.Subscription(sub); sub.Status.InstalledCSV != targetChannel.CurrentCSVName; {
+		fmt.Printf("Installed csv: %s, Current CSV: %s\n", sub.Status.InstalledCSV, targetChannel.CurrentCSVName)
+		testKube.WaitForSubscriptionState(coreos.SubscriptionStateUpgradePending, sub)
+		testKube.ApproveInstallPlan(sub)
+
+		testKube.WaitForSubscription(sub, func() bool {
+			return sub.Status.InstalledCSV == sub.Status.CurrentCSV
+		})
+
+		// Check migration
+		currentStatefulSetName := clusterName
+		newStatefulSetName := clusterName + "-1"
+
+		testKube.WaitForStateFulSet(newStatefulSetName, tutils.Namespace)
+		testKube.WaitForStateFulSetRemoval(currentStatefulSetName, tutils.Namespace)
+		testKube.WaitForInfinispanPodsCreatedBy(numPods, tutils.SinglePodTimeout, newStatefulSetName, tutils.Namespace)
+		testKube.WaitForInfinispanPodsCreatedBy(0, tutils.SinglePodTimeout, currentStatefulSetName, tutils.Namespace)
+
+		if !tutils.CheckExternalAddress(client) {
+			panic("Error contacting server")
+		}
+
+		// Check data
+		assert.Equal(t, entriesPerCache, cacheSize("textCache", client))
+		assert.Equal(t, entriesPerCache, cacheSize("indexedCache", client))
+		assert.Equal(t, 0, cacheSize("jsonCache", client))
+		assert.Equal(t, 0, cacheSize("javaCache", client))
+	}
 }
 
 func cacheSize(cacheName string, client tutils.HTTPClient) int {
@@ -165,5 +137,5 @@ func addData(cacheName string, entries int, client tutils.HTTPClient) {
 		data := strconv.Itoa(i)
 		cache.Put(data, data, mime.TextPlain)
 	}
-	fmt.Printf("Populated cache %s with %d entries", cacheName, entries)
+	fmt.Printf("Populated cache %s with %d entries\n", cacheName, entries)
 }
