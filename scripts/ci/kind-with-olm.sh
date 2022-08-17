5de69f45db578e75ffd3fcaac65c46d99921fd7e
Ryan Emerson <remerson@redhat.com>
2022-08-17 09:59:30 +0100

    Utilise OLM v0.21.2
    latest` v0.22.0 GitHub release is missing required crd.yaml files
    causing installation to fail via `operator-sdk olm install`
    
    https://github.com/operator-framework/operator-lifecycle-manager/issues/2841

--- scripts/ci/kind-with-olm.sh
+++ scripts/ci/kind-with-olm.sh
@@ -3,9 +3,11 @@ set -o errexit
 
 SCRIPT_DIR=$(dirname "$0")
 
+OLM_VERSION="v0.21.2"
+
 source $SCRIPT_DIR/kind.sh
 
-operator-sdk olm install
+operator-sdk olm install --version ${OLM_VERSION}
 
 # Sometimes olm install does not wait long enough for deployments to be rolled out
 kubectl wait --for=condition=available --timeout=60s deployment/catalog-operator -n olm
