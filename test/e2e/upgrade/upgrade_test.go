deeeafc6b42eba64c473754932dacd8552ede842
Ryan Emerson <remerson@redhat.com>
2023-09-25 16:01:22 +0100

    Ignore Restore failures for issues on older Operands
    Failures ignored:
    
    - org.infinispan.CONFIG timeout due to ISPN-15181
    - Split-brains due to ISPN-15089
    - Incompatible example.PROTOBUF_DIST template when upgrading to 14.0.9

--- test/e2e/upgrade/upgrade_test.go
+++ test/e2e/upgrade/upgrade_test.go
@@ -4,21 +4,25 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/blang/semver"
 	ispnv1 "github.com/infinispan/infinispan-operator/api/v1"
 	v2 "github.com/infinispan/infinispan-operator/api/v2alpha1"
 	"github.com/infinispan/infinispan-operator/controllers/constants"
 	"github.com/infinispan/infinispan-operator/pkg/infinispan/version"
 	"github.com/infinispan/infinispan-operator/pkg/mime"
+	"github.com/infinispan/infinispan-operator/pkg/reconcile/pipeline/infinispan/handler/provision"
 	batchtest "github.com/infinispan/infinispan-operator/test/e2e/batch"
 	tutils "github.com/infinispan/infinispan-operator/test/e2e/utils"
 	coreos "github.com/operator-framework/api/pkg/operators/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 var (
@@ -86,10 +90,17 @@ func TestUpgrade(t *testing.T) {
 
 	// Add a volatile cache with data to ensure contents can be backed up and then restored after upgrade(s)
 	volatileCache := "volatileCache"
-	cache = tutils.NewCacheHelper(volatileCache, client)
-	cache.Create(`{"distributed-cache":{"mode":"SYNC"}}`, mime.ApplicationJson)
-	cache.Populate(numEntries)
-	cache.AssertSize(numEntries)
+	createAndPopulateVolatileCache := func(client tutils.HTTPClient) {
+		c := tutils.NewCacheHelper(volatileCache, client)
+		if !c.Exists() {
+			c.Create(`{"distributed-cache":{"mode":"SYNC"}}`, mime.ApplicationJson)
+		}
+		if c.Size() != numEntries {
+			c.Populate(numEntries)
+			c.AssertSize(numEntries)
+		}
+	}
+	createAndPopulateVolatileCache(client)
 
 	// Create Backup
 	backup := &v2.Backup{
@@ -227,7 +238,15 @@ func TestUpgrade(t *testing.T) {
 			},
 		}
 		testKube.Create(restore)
-		testKube.WaitForValidRestorePhase(restore.Name, restore.Namespace, v2.RestoreSucceeded)
+
+		if restore, err := waitForValidRestorePhase(restore.Name, restore.Namespace, v2.RestoreSucceeded); err != nil {
+			tutils.ExpectNoError(
+				ignoreRestoreError(sub.Status.InstalledCSV, &latestOperand, spec, restore, client, err),
+			)
+			// We must recreate the caches that should have been restored if the Restore CR had succeeded
+			// so that the Backup CR executed in the next loop has the expected content
+			createAndPopulateVolatileCache(client)
+		}
 		tutils.NewCacheHelper(volatileCache, client).AssertSize(numEntries)
 	}
 
@@ -248,6 +267,95 @@ func TestUpgrade(t *testing.T) {
 	// Ensure that persistent cache entries still contain the expected numEntries
 	client = tutils.HTTPClientForCluster(spec, testKube)
 	tutils.NewCacheHelper(peristentCache, client).AssertSize(numEntries)
+}
+
+func waitForValidRestorePhase(name, namespace string, phase v2.RestorePhase) (restore *v2.Restore, err error) {
+	err = wait.Poll(10*time.Millisecond, tutils.TestTimeout, func() (bool, error) {
+		restore = testKube.GetRestore(name, namespace)
+		if restore.Status.Phase == v2.RestoreFailed {
+			return true, fmt.Errorf("restore failed. Reason: %s", restore.Status.Reason)
+		}
+		return phase == restore.Status.Phase, nil
+	})
+	if err != nil {
+		err = fmt.Errorf("Expected Restore Phase %s, got %s:%s", phase, restore.Status.Phase, restore.Status.Reason)
+	}
+	return
+}
+
+// ignoreRestoreError returns nil if the Restore failure can safely be ignored for the given Operand
+func ignoreRestoreError(csv string, operand *version.Operand, ispn *ispnv1.Infinispan, restore *v2.Restore, client tutils.HTTPClient, err error) error {
+	fmt.Println(err)
+	switch restore.Status.Phase {
+	case v2.RestoreInitialized:
+	case v2.RestoreRunning:
+	case v2.RestoreSucceeded:
+	case v2.RestoreUnknown:
+		return err
+	}
+
+	if restore.Status.Phase == v2.RestoreInitializing {
+		// Ignore errors where the Restore state is stuck initializing due to a MERGED cluster view from PublishNotReadyAddresses=false
+		// Fixed in Operator 2.3.2, but ISPN-14793 should fix this for Operands >= 14.0.9 even with PublishNotReadyAddresses=false
+		if semver.MustParse(csv).LT(semver.Version{Major: 2, Minor: 3, Patch: 2}) {
+			return nil
+		}
+		return err
+	}
+
+	// Ignore split brain errors on older Operands that are caused by one of the cluster members
+	// restarting due to an incompatible configuration caused by ISPN-15089 and ISPN-15191
+	if operand.UpstreamVersion.LT(semver.Version{Major: 14, Minor: 0, Patch: 17}) &&
+		strings.Contains(restore.Status.Reason, "Key 'ClusteredLockKey{name=BackupManagerImpl-restore}' is not available. Not all owners are in this partition") {
+		// Check the restartCount and logs of the cluster pods to ensure ISPN-15089 is the cause of
+		// a server pod restarting
+		podList := &corev1.PodList{}
+		tutils.ExpectNoError(
+			testKube.Kubernetes.ResourcesList(restore.Namespace, map[string]string{"app": "infinispan-pod", "infinispan_cr": ispn.Name}, podList, ctx),
+		)
+
+		r := regexp.MustCompile("FATAL.*ISPN080028.*GlobalConfigurationManager failed to start")
+		for _, pod := range podList.Items {
+			for _, c := range pod.Status.ContainerStatuses {
+				if c.Name == provision.InfinispanContainer && c.RestartCount > 0 {
+					logs, err := testKube.Kubernetes.Logs(pod.Name, pod.Namespace, true, ctx)
+					tutils.ExpectNoError(err)
+
+					if r.MatchString(logs) {
+						fmt.Printf("Ignoring Restore failure caused by ISPN-15089 on Operand %s\n", operand)
+						// Force the org.infinispan.LOCKS cache to become available so that subsequent upgrades will proceed
+						tutils.NewCacheHelper("org.infinispan.LOCKS", client).Available(true)
+						return nil
+					}
+				}
+			}
+		}
+	}
+
+	if strings.Contains(restore.Status.Reason, "EOF") {
+		logs, err := testKube.Kubernetes.Logs(restore.Name, restore.Namespace, false, ctx)
+		tutils.ExpectNoError(err)
+
+		if operand.UpstreamVersion.LT(semver.Version{Major: 14, Minor: 0, Patch: 18}) {
+			// Ignore ISPN-15181 related errors on older Operands
+			r := regexp.MustCompile(`Error executing command PutKeyValueCommand on Cache 'org.infinispan.CONFIG'.*org.infinispan.util.concurrent.TimeoutException: ISPN000476`)
+			if r.MatchString(logs) {
+				fmt.Printf("Ignoring Restore failure caused by ISPN-15181 on Operand %s\n", operand)
+				return nil
+			}
+		}
+
+		if operand.UpstreamVersion.EQ(semver.Version{Major: 14, Minor: 0, Patch: 9}) {
+			// Ignore example.PROTOBUF_DIST incompatibility. Users can workaround this issue by excluding the example.PROTOBUF_DIST
+			// template from their Restore CR
+			r := regexp.MustCompile(`ISPN000961: Incompatible attribute 'media-type.example.PROTOBUF_DIST.distributed-cache-configuration.encoding'`)
+			if r.MatchString(logs) {
+				fmt.Printf("Ignoring Restore failure caused by example.PROTOBUF_DIST template encoding incompatibility %s\n", operand)
+				return nil
+			}
+		}
+	}
+	return err
 }
 
 func checkServicePorts(t *testing.T, name string) {
