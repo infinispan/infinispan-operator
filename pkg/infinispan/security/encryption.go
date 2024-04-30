b594c0863a84b3ba3f8d5b29000d4abe9bce8b4a
Pavel Drobek <pdrobek@pdrobek-thinkpadp1gen4i.remote.csb>
2024-04-30 11:29:10 +0200

    Use modern encryption algorithms for truststore

--- pkg/infinispan/security/encryption.go
+++ pkg/infinispan/security/encryption.go
@@ -1,7 +1,6 @@
 package security
 
 import (
-	"crypto/rand"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
@@ -33,7 +32,7 @@ func GenerateTruststore(pemFiles [][]byte, password string) ([]byte, error) {
 			pemRaw = rest
 		}
 	}
-	truststore, err := p12.EncodeTrustStore(rand.Reader, certs, password)
+	truststore, err := p12.Modern2023.EncodeTrustStore(certs, password)
 	if err != nil {
 		return nil, fmt.Errorf("Unable to create truststore with user provided cert files: %w", err)
 	}
