46955e8c7a84ef1c58740be73651c089ab1a1b02
Ryan Emerson <remerson@redhat.com>
2024-10-10 14:45:39 +0100

    Next version 2.4.6

--- scripts/ci/install-catalog-source.sh
+++ scripts/ci/install-catalog-source.sh
@@ -7,7 +7,7 @@ IMG_REGISTRY=${IMG_REGISTRY-"localhost:5001"}
 
 export CHANNELS=stable
 export DEFAULT_CHANNEL=stable
-export VERSION=2.4.5
+export VERSION=2.4.6
 
 BUNDLE_IMG_NAME=infinispan-operator-bundle
 
