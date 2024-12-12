31e79944d7fd4201d4c8d2b1d05dc8c3f904f288
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2024-12-12 21:00:34 +0100

    Next version 2.4.7

--- scripts/ci/install-catalog-source.sh
+++ scripts/ci/install-catalog-source.sh
@@ -7,7 +7,7 @@ IMG_REGISTRY=${IMG_REGISTRY-"localhost:5001"}
 
 export CHANNELS=stable
 export DEFAULT_CHANNEL=stable
-export VERSION=2.4.6
+export VERSION=2.4.7
 
 BUNDLE_IMG_NAME=infinispan-operator-bundle
 
