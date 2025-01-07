infinispan/infinispan-operator

commit 827de542f56a3909159ee7125b7646d0853f3656

Attempt to use podman in scripts if docker not installed

scripts/ci/configure-xsite.sh
@@ -17,6 +17,13 @@ kind delete clusters --all
 # Common part for both nodes
 make operator-build IMG=$IMG
 
+if ! [ -x "$(docker -v)" ]; then
+    echo "docker not installed, trying podman"
+    function docker() {
+        podman "$@"
+    }
+fi
+
 # Create the Kind network with subnet.
 docker network rm kind || true
 docker network create kind --subnet "${KIND_SUBNET}/16"

scripts/ci/kind.sh
@@ -5,6 +5,13 @@ set -o errexit
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/operand_common.sh"
 
+if ! [ -x "$(docker -v)" ]; then
+    echo "docker not installed, trying podman"
+    function docker() {
+        podman "$@"
+    }
+fi
+
 if [[ -z "${SERVER_IMAGES}" ]]; then
   SERVER_IMAGES=$(operandJson | jq -r '.[].image')
 fi

commit 1d8f8d883e50029acc3965bf31ab7ec37c8e332b

Fix issues with kind.sh introduced by 827de54
* Change the condition to verify docker daemon
* Restart kind registry container if it already exists but is stopped

---------

Co-authored-by: Ryan Emerson <remerson@redhat.com>

scripts/ci/configure-xsite.sh
@@ -17,7 +17,7 @@ kind delete clusters --all
 # Common part for both nodes
 make operator-build IMG=$IMG
 
-if ! [ -x "$(docker -v)" ]; then
+if ! command -v docker &> /dev/null; then
     echo "docker not installed, trying podman"
     function docker() {
         podman "$@"

scripts/ci/kind.sh
@@ -5,7 +5,7 @@ set -o errexit
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/operand_common.sh"
 
-if ! [ -x "$(docker -v)" ]; then
+if ! command -v docker &> /dev/null; then
     echo "docker not installed, trying podman"
     function docker() {
         podman "$@"
@@ -25,11 +25,11 @@ docker network create kind --subnet "${KIND_SUBNET}/16" || true
 # create registry container unless it already exists
 reg_name='kind-registry'
 reg_port=${KIND_PORT-'5001'}
-running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
-if [ "${running}" != 'true' ]; then
-  docker run \
-    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
-    ${DOCKER_REGISTRY_IMAGE}
+reg_running="$(docker container inspect -f '{{.State.Running}}' ${reg_name} || echo 'create' | xargs)"
+if [ $reg_running = "create" ]; then
+  docker run -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" "${DOCKER_REGISTRY_IMAGE}"
+elif [ $reg_running = "false" ]; then
+  docker container restart ${reg_name}
 fi
 
 # create a cluster with the local registry enabled in containerd