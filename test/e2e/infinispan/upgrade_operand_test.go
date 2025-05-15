ca538ad1d43d0094215748789f815e235ec2da26
Ryan Emerson <remerson@redhat.com>
2025-05-15 18:01:28 +0100

    [#2269] GracefulShutdown upgrades should tolerate pods that have already been stopped

--- test/e2e/infinispan/upgrade_operand_test.go
+++ test/e2e/infinispan/upgrade_operand_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	ispnv1 "github.com/infinispan/infinispan-operator/api/v1"
+	"github.com/infinispan/infinispan-operator/pkg/infinispan/client"
 	"github.com/infinispan/infinispan-operator/pkg/infinispan/version"
 	"github.com/infinispan/infinispan-operator/pkg/kubernetes"
 	"github.com/infinispan/infinispan-operator/pkg/mime"
@@ -407,6 +408,51 @@ func TestSpecImageUpdate(t *testing.T) {
 	assert.EqualValues(t, 1, ss.Status.ObservedGeneration)
 }
 
+// TestPodAlreadyShutdownOnUpgrade simulates a scenario where a GracefulShutdown fails when only a subset of pods have had
+// their container shutdown
+func TestPodAlreadyShutdownOnUpgrade(t *testing.T) {
+	defer testKube.CleanNamespaceAndLogOnPanic(t, tutils.Namespace)
+
+	i := tutils.DefaultSpec(t, testKube, func(infinispan *ispnv1.Infinispan) {
+		infinispan.Spec.Replicas = 1
+		infinispan.Spec.Security.EndpointAuthentication = pointer.Bool(false)
+	})
+	testKube.CreateInfinispan(i, tutils.Namespace)
+	testKube.WaitForInfinispanPods(1, tutils.SinglePodTimeout, i.Name, tutils.Namespace)
+	i = testKube.WaitForInfinispanCondition(i.Name, i.Namespace, ispnv1.ConditionWellFormed)
+
+	schema := i.GetEndpointScheme()
+	client_ := testKube.WaitForExternalService(i, tutils.RouteTimeout, tutils.NewHTTPClientNoAuth(schema), nil)
+	ispnClient := client.New(tutils.CurrentOperand, client_)
+	tutils.ExpectNoError(
+		ispnClient.Container().Shutdown(),
+	)
+
+	tutils.ExpectNoError(
+		testKube.UpdateInfinispan(i, func() {
+			i.Spec.Replicas = 0
+		}),
+	)
+	testKube.WaitForInfinispanPods(0, tutils.SinglePodTimeout, i.Name, tutils.Namespace)
+
+	ss := appsv1.StatefulSet{}
+	tutils.ExpectNoError(
+		testKube.Kubernetes.Client.Get(
+			context.TODO(),
+			types.NamespacedName{
+				Namespace: i.Namespace,
+				Name:      i.GetStatefulSetName(),
+			},
+			&ss,
+		),
+	)
+	assert.EqualValues(t, int64(2), ss.Status.ObservedGeneration)
+	assert.EqualValues(t, int32(0), ss.Status.ReadyReplicas)
+	assert.EqualValues(t, int32(0), ss.Status.CurrentReplicas)
+	assert.EqualValues(t, int32(0), ss.Status.UpdatedReplicas)
+}
+
+// specImageOperands() returns two latest Operands with the matching major/minor version
 func specImageOperands() (*version.Operand, *version.Operand) {
 	operands := tutils.VersionManager().Operands
 	length := len(operands)
