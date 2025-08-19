be35588c3491ef853b4e5dd264c1edc3060cd9a0
Priyanka Minz <pminz@li-a4883acc-2255-11b2-a85c-bc4b81ccc785.ibm.com>
2025-08-19 20:00:45 +0530

    Next Version 2.4.17

--- scripts/ci/install-catalog-source.sh
+++ scripts/ci/install-catalog-source.sh
@@ -7,7 +7,7 @@ IMG_REGISTRY=${IMG_REGISTRY-"localhost:5001"}
 
 export CHANNELS=stable
 export DEFAULT_CHANNEL=stable
-export VERSION=2.4.16
+export VERSION=2.4.17
 
 BUNDLE_IMG_NAME=infinispan-operator-bundle
 
