3ac5062b732afb9a063752b17698d0e85edb1234
Ryan Emerson <remerson@ibm.com>
2026-01-02 11:14:36 +0000

    [#2410] CI unable to create kind cluster due to 'no space left on device'
    - Only test oldest and latest releases in 16.0.x stream
    - Explicilty free disk space for webhook tests as they require OLM

--- scripts/ci/install_test_operands.sh
+++ scripts/ci/install_test_operands.sh
@@ -10,21 +10,28 @@ source "${SCRIPT_DIR}/operand_common.sh"
 
 current_versions=$(operandJson)
 
-# Test the latest release from each previous stable streams in order to reduce resources and time required by CI
-# We explicitly include 14.0.24 so that we can continue to test upgrades from 2.3.x CSV bundles
+# Test the latest release from each previous stable stream in order to reduce resources and time required by CI
 test_operands=$(echo "${current_versions}" | jq '
-[
-  {
-    "upstream-version": "14.0.24",
-    "image": "quay.io/infinispan/server:14.0.24.Final"
-  },
-  (map(select(.["upstream-version"] | startswith("14.0."))) | sort_by(.["upstream-version"] | split(".") | map(tonumber)) | .[-1]),
-  (map(select(.["upstream-version"] | startswith("15.0."))) | sort_by(.["upstream-version"] | split(".") | map(tonumber)) | .[-1]),
-  (map(select(.["upstream-version"] | startswith("15.1."))) | sort_by(.["upstream-version"] | split(".") | map(tonumber)) | .[-1]),
-  (map(select(.["upstream-version"] | startswith("15.2."))) | sort_by(.["upstream-version"] | split(".") | map(tonumber)) | .[-1]),
-  (map(select(.["upstream-version"] | startswith("16.")))   | sort_by(.["upstream-version"] | split(".") | map(tonumber)) | .[])
-] | map(select(. != null))
+ # 1. Group by Major.Minor
+ group_by(.["upstream-version"] | sub("^(?<a>\\d+\\.\\d+).*"; .a)) as $groups
+
+ # 2. Identify the "Oldest of the Latest Stream"
+ # We take the last group, then the first element of that group
+ | ($groups | last | first | .["upstream-version"]) as $oldestLatest
+
+ # 3. Process the groups
+ | $groups
+ | map(
+     . as $group
+     | .[]
+     # Test both the oldest and the latest patch release in the latest major.minor stream
+     | select(. == ($group | last) or .["upstream-version"] == $oldestLatest)
+   )
+ | unique_by(.["upstream-version"])
 ')
+
+# Explicitly include 14.0.24 so that we can continue to test upgrades from 2.3.x CSV bundles
+test_operands=$(echo "${test_operands}" | jq '[{"upstream-version": "14.0.24", "image": "quay.io/infinispan/server:14.0.24.Final"}] + .')
 
 # Append Dev version only if there's no release in the stream yet
 if [ $(echo ${test_operands} | jq 'map(select(.["upstream-version"] | startswith("16."))) | length') -eq 0 ]; then
