dba80c1650e7b86de2caf9942d95ef4ea40e850b
Pavel Drobek <pdrobek@ibm.com>
2026-04-23 16:44:39 +0200

    [#2324] Allow to specify environment variables per CR

--- test/e2e/infinispan/env_vars_test.go
+++ test/e2e/infinispan/env_vars_test.go
@@ -52,11 +52,71 @@ func TestAdditionalVarsUpdated(t *testing.T) {
 	verifyStatefulSetUpdate(*spec, modifier, verifier)
 }
 
+// TestEnvVarPropagation verifies that:
+// 1. Literal EnvVars from the Infinispan CR are correctly synced to Pods.
+// 2. Changes to ValueFrom references update the pod template hash, triggering a StatefulSet rollout.
+func TestEnvVarsPropagation(t *testing.T) {
+	defer testKube.CleanNamespaceAndLogOnPanic(t, tutils.Namespace)
+
+	testEnvName := "TEST_ENV"
+	testEnvValue := "test_value"
+	testEnvFromName := "FROM_VALUE_ENV"
+
+	getEnvVars := func(fieldPath string) []corev1.EnvVar {
+		return []corev1.EnvVar{
+			{
+				Name:  testEnvName,
+				Value: testEnvValue,
+			},
+			{
+				Name: testEnvFromName,
+				ValueFrom: &corev1.EnvVarSource{
+					FieldRef: &corev1.ObjectFieldSelector{
+						FieldPath: fieldPath,
+					},
+				},
+			},
+		}
+	}
+
+	spec := tutils.DefaultSpec(t, testKube, func(i *ispnv1.Infinispan) {
+		i.Spec.Container.Env = getEnvVars("metadata.namespace")
+	})
+
+	testKube.CreateInfinispan(spec, tutils.Namespace)
+	testKube.WaitForInfinispanCondition(spec.Name, spec.Namespace, ispnv1.ConditionWellFormed)
+
+	podList := testKube.WaitForInfinispanPods(1, tutils.SinglePodTimeout, spec.Name, tutils.Namespace)
+
+	assert.Equal(t, testEnvValue, getEnvVar(testEnvName, &podList.Items[0].Spec))
+	assert.Equal(t, "metadata.namespace", getValueFromFieldPath(testEnvFromName, &podList.Items[0].Spec))
+
+	// Update the EnvFromValue, if the value is propagated the hash has been updated
+	var modifier = func(ispn *ispnv1.Infinispan) {
+		ispn.Spec.Container.Env = getEnvVars("spec.serviceAccountName")
+	}
+	var verifier = func(ispn *ispnv1.Infinispan, ss *appsv1.StatefulSet) {
+		assert.Equal(t, testEnvValue, getEnvVar(testEnvName, &ss.Spec.Template.Spec))
+		assert.Equal(t, "spec.serviceAccountName", getValueFromFieldPath(testEnvFromName, &ss.Spec.Template.Spec))
+	}
+	verifyStatefulSetUpdate(*spec, modifier, verifier)
+}
+
 func getEnvVar(env string, podSpec *corev1.PodSpec) string {
 	container := kube.GetContainer(provision.InfinispanContainer, podSpec)
 	for _, e := range container.Env {
 		if env == e.Name {
 			return e.Value
+		}
+	}
+	panic(fmt.Sprintf("ENV '%s' not found in container", env))
+}
+
+func getValueFromFieldPath(env string, podSpec *corev1.PodSpec) string {
+	container := kube.GetContainer(provision.InfinispanContainer, podSpec)
+	for _, e := range container.Env {
+		if env == e.Name {
+			return e.ValueFrom.FieldRef.FieldPath
 		}
 	}
 	panic(fmt.Sprintf("ENV '%s' not found in container", env))
